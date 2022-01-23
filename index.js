06d66998151ebaf2935c38ff2a7508e883000b84
Emre Ünsal <unsalemre111@gmail.com>
2022-01-23 18:44:30 +0300

    add url shortener path

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require("express");
+const nanoid = require("nanoid");
 
 const app = express();
 app.use(express.json());
 
-let users = [];
-
 app.use("/", express.static("public"));
 
-app.post("/users", (request, response) => {
-  users.push(request.body);
-  response.send(users);
+const urls = [];
+
+app.post("/api/url", (req, res) => {
+  const { url } = req.body;
+  const slug = nanoid.nanoid(5);
+  const data = { slug, url };
+  urls.push(data);
+  res.send(data);
 });
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
-
-app.get("/users/:name", (req, res) => {
-  const user = users.find((user) => user.name === req.params.name);
-  res.send(user);
+app.get("/:slug", (req, res) => {
+  const { slug } = req.params;
+  const link = urls.find((url) => url.slug === slug);
+  res.redirect(link.url);
 });
 
 app.listen(3000, () => {});
--- public/index.js
+++ public/index.js
@@ -1,4 +1,19 @@
 const main = () => {
-  const url = document.querySelector("#urlText");
+  const apiURL = window.location.host;
+  const urlInput = document.querySelector("#urlText");
+  const button = document.querySelector("#button");
+  button.addEventListener("click", async () => {
+    const url = urlInput.value;
+    const body = { url };
+    const response = await fetch(`${apiURL}/api/url`, {
+      body: JSON.stringify(body),
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  });
 };
 window.addEventListener("load", main);
