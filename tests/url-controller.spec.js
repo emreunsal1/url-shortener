cc102f5bc44925611172efea195c394081b2e04c
Emre Ünsal <unsalemre111@gmail.com>
2022-01-30 22:50:42 +0300

    add controller tests

--- tests/url-controller.spec.js
+++ tests/url-controller.spec.js
@@ -1,17 +1,98 @@
-const { addUrlController } = require("../controllers/url-controllers");
+const {
+  addUrlController,
+  redirectUrlController,
+} = require("../controllers/url-controllers");
+
+const errorResponse = {
+  error: true,
+};
+
+const NANOID_RESPONSE = "NANOID_RESPONSE";
+const GETURLBYSLUG_RESPONSE = { url: "a" };
 
 jest.mock("../models/url", () => ({
-  addUrl: jest.fn(() => "response"),
+  addUrl: jest.fn((url, slug) => {
+    if (url == "error") {
+      return errorResponse;
+    }
+    return {
+      url,
+      slug,
+    };
+  }),
+
+  getUrlBySlug: jest.fn(() => {
+    return GETURLBYSLUG_RESPONSE;
+  }),
 }));
 
+jest.mock("nanoid", () => ({
+  nanoid: jest.fn(() => NANOID_RESPONSE),
+}));
+
+const requestFactory = (override = {}) => {
+  return {
+    body: { url: "oley" },
+    ...override,
+  };
+};
+
 describe("addUrlController", () => {
-  it("should call res.send function with correct parameters", async () => {
-    const req = { body: { url: "oley" } };
-    const mockSend = jest.fn();
-    const res = { send: mockSend };
+  const mockSend = jest.fn();
+  const mockSendStatus = jest.fn();
+  let req;
+  let res;
+
+  beforeEach(() => {
+    req = requestFactory();
+    res = { send: mockSend, sendStatus: mockSendStatus };
+  });
+  it("should call res.sendStatus with ", async () => {
+    await addUrlController(req, res);
+
+    expect(mockSend.mock.calls[0][0]).toStrictEqual({
+      url: req.body.url,
+      slug: NANOID_RESPONSE,
+    });
+  });
+  it("should call res.sendStatus with 400 when addUrl function returns error", async () => {
+    req = requestFactory({ body: { url: "error" } });
 
     await addUrlController(req, res);
 
-    expect(mockSend.mock.calls[0][0]).toBe("response");
+    expect(mockSendStatus.mock.calls[0][0]).toBe(400);
+  });
+});
+
+describe("redirectUrlController", () => {
+  const mockSend = jest.fn();
+  const mockSendStatus = jest.fn();
+  const mockRedirect = jest.fn();
+  let req;
+  let res;
+
+  beforeEach(() => {
+    req = requestFactory();
+    res = {
+      redirect: mockRedirect,
+      send: mockSend,
+      sendStatus: mockSendStatus,
+    };
+  });
+
+  it("should call res.redirect with data.url", async () => {
+    req = { params: { slug: "abc" } };
+
+    await redirectUrlController(req, res);
+
+    expect(mockRedirect.mock.calls[0][0]).toBe(GETURLBYSLUG_RESPONSE.url);
+  });
+
+  it("should call res.sendStatus with 404 when req.params.slug favicon.ico", async () => {
+    req = { params: { slug: "favicon.ico" } };
+
+    await redirectUrlController(req, res);
+
+    expect(mockSendStatus.mock.calls[0][0]).toBe(404);
   });
 });
